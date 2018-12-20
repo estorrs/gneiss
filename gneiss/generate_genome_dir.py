cad897f010677fad96a1b9eedfd74796fc67885e
Erik Storrs <estorrs@katmai.wusm.wustl.edu>
2018-12-20 17:49:48 -0600

    working gneiss wrapper for star aligner

---
+++ gneiss/generate_genome_dir.py
@@ -0,0 +1,65 @@
+import argparse
+import os
+import subprocess
+
+"""
+usage: generate_genome_dir.py genome_dir_fp [options]
+
+args:
+genome_dir_name: str
+    name of genome directory
+
+options:
+--threads: int
+    how many processes to allow STAR to use
+--reference_fasta: str
+    reference fasta
+--gtf: str
+    gtf file
+"""
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('genome_dir_fp', type=str,
+        help='what to name output genome dir')
+
+reference_group = parser.add_argument_group('reference_group')
+reference_group.add_argument('--reference-fasta', type=str,
+        help='location of reference fasta')
+
+parser.add_argument('--threads', type=int,
+        default=1, help='how many processes to allow tophat and samtools to use')
+parser.add_argument('--gtf', type=str,
+        help='location of gtf file')
+
+args = parser.parse_args()
+
+def check_arguments():
+    if args.reference_fasta is None:
+        raise ValueError('Must specify a bowtie2 compatible reference fasta')
+
+def generate_genome_dir(genome_dir_fp, reference_fp, gtf_fp, threads=1):
+    # make sure genome_dir exists
+    if not os.path.exists(genome_dir_fp):
+        os.makedirs(genome_dir_fp)
+
+    tool_args = ['STAR',
+            '--runThreadN', str(threads),
+            '--runMode', 'genomeGenerate',
+            '--genomeDir', genome_dir_fp,
+            '--genomeFastaFiles', reference_fp]
+    
+    if gtf_fp is not None:
+        tool_args += ['--sjdbGTFfile', gtf_fp]
+
+    print(f'Running the following: {" ".join(tool_args)}')
+    print(subprocess.check_output(tool_args).decode('utf-8'))
+
+def main():
+    check_arguments()
+    
+    generate_genome_dir(args.genome_dir_fp, args.reference_fasta, args.gtf, args.threads)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+
+import pytest
+
+TEST_DATA_DIR = 'tests/data/'
+TEST_FASTA_REFERENCE = TEST_DATA_DIR + 'test_ref.fa'
+TEST_GENOME_DIR = TEST_DATA_DIR + 'genome_dir'
+TEST_OUTPUT_DIR = TEST_DATA_DIR + 'outputs'
+
+TEST_READ_1 = TEST_DATA_DIR + 'reads_1.fq'
+TEST_READ_2 = TEST_DATA_DIR + 'reads_2.fq'
+
+def test_build_genome_dir():
+    tool_args = ['python', 'gneiss/generate_genome_dir.py',
+            '--threads', '2',
+            '--reference-fasta', TEST_FASTA_REFERENCE,
+            TEST_GENOME_DIR]
+    
+    results = subprocess.check_output(tool_args).decode('utf-8')
+
+    assert 'finished successfully' in results
+
+def test_star_aligner():
+    tool_args = ['python', 'gneiss/gneiss.py',
+            '--threads', '2',
+            '--output-dir', TEST_OUTPUT_DIR,
+            '--genome-dir', TEST_GENOME_DIR,
+            TEST_READ_1, TEST_READ_2]
+    
+    results = subprocess.check_output(tool_args).decode('utf-8')
+
+    assert 'finished successfully' in results
+            
+    
