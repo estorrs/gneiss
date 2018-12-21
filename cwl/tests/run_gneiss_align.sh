4b2b5562b6520a8953e82d61e804c2691e34eb3d
Erik Storrs <estorrs@katmai.wusm.wustl.edu>
2018-12-21 13:19:28 -0600

    added test cwl for generate genome dir

--- cwl/tests/run_gneiss_align.sh
+++ cwl/tests/run_gneiss_align.sh
@@ -3,7 +3,7 @@
 CWL="/diskmnt/Projects/Users/estorrs/gneiss/cwl/gneiss_align.cwl"
 YAML="/diskmnt/Projects/Users/estorrs/gneiss/cwl/tests/gneiss_align_config.yaml"
 
-mkdir -p /diskmnt/Projects/Users/estorrs/gneiss/cwl/tests/test_results
-RABIX_ARGS="--basedir /diskmnt/Projects/Users/estorrs/gneiss/cwl/tests/test_results"
+mkdir -p /diskmnt/Projects/Users/estorrs/gneiss/cwl/tests/test_results/align
+RABIX_ARGS="--basedir /diskmnt/Projects/Users/estorrs/gneiss/cwl/tests/test_results/align"
 
 rabix $RABIX_ARGS $CWL $YAML
